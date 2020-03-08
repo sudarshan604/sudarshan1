058de251046f3c5e14353b36d987396bd7ebc838
sudarshan kshetri <sudarshadn1999@gmail.com>
2020-03-08 16:54:58 +0545

    third commit

--- multiply.c
+++ multiply.c
@@ -1,2 +1,11 @@
 #include<stdio.h>
 #include<conio.h>
+void main()
+{
+    int a,b;
+    printf("enter first number");
+    scanf("%d",&a);
+    printf("enter second  number");
+    scanf("%d",&b);
+
+}
